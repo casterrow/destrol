7a63e8b6d266da6c1c68d14997ac52106ed4bcce
Stephen <ste7en.liu@gmail.com>
2017-07-05 14:10:32 +0800

    Source code

---
+++ destrol-boot/src/main/java/com/github/destrol/DestrolStartup.java
@@ -0,0 +1,17 @@
+package com.github.destrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Stephen Liu
+ * @since 2017/7/5
+ */
+@SpringBootApplication
+public class DestrolStartup {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(DestrolStartup.class, args);
+    }
+}
